bee0b5d9c76d33e27be117f4e08508fba7971a2f
Andrey Trachenko <goodrone@users.noreply.github.com>
2019-01-05 16:29:42 +0300

    first working version

--- index.js
+++ index.js
@@ -2,50 +2,87 @@ import './style';
 import { Component } from 'preact';
 import linkState from 'linkState';
 
-const getRandom = () => {
-    return Math.floor(Math.random() * 9) + 1;
+const getRandom = (a, b) => {
+    return Math.floor(Math.random() * (b - a + 1)) + a;
 }
 
-const PlayButton = (onClick) => {
-    return <button type="button" onClick={onClick}>Play</button>;
+const PlayButton = ({ onClick }) => {
+    return <button type="button" onClick={onClick}>▶</button>;
 };
 
 const getRange = n => Array.from(Array(n).keys());
 
-export default class App extends Component {
+const Figures = ({ number }) => {
+    return (
+        <div>{getRange(number).map(() => {
+            return "⚫";
+        })}</div>
+    );
+}
+
+const BigSign = ({ symbol }) => {
+    return <div className="display">{symbol}</div>;
+}
+
+const Success = () => <BigSign symbol="✓"/>;
+const Fail = () => <BigSign symbol="&#x274C;"/>;
+
+class Game extends Component {
     constructor(props) {
         super(props);
         this.guess = this.guess.bind(this);
+        this.start = this.start.bind(this);
     }
     guess(value) {
         console.assert(this.state.number !== undefined);
-        if (value === this.state.number) {
-            this.setState({fullscreen: 'success', number: undefined});
-        } else {
-            this.setState({fullscreen: 'fail', number: undefined});
-        }
+        this.setState({
+            fullscreen: value === this.state.number ? 'success' : 'fail',
+            number: undefined,
+        });
     }
-    render({}, { number, fullscreen }) {
+    start() {
+        this.setState({
+            fullscreen: undefined,
+            number: getRandom(1, 5),
+        });
+    }
+    renderInner(number) {
         if (number === undefined) {
             return (
-                <div>
-                {fullscreen !== undefined && fullscreen}
-                <PlayButton onClick={() => this.setState({number: getRandom()})}/>
+                <div className="buttons">
+                    <PlayButton onClick={this.start}/>
                 </div>
             );
         }
         return (
             <div>
-                <h1>{number}</h1>
-                {getRange(9).map(i => {
-                    const value = i + 1;
-                    return (
-                        <button type="button" onClick={() => this.guess(value)}>
-                            {value}
-                        </button>
-                    );
-                })}
+                <div className="display"><Figures number={number}/></div>
+                <div className="buttons">
+                    {getRange(9).map(i => {
+                        const value = i + 1;
+                        return (
+                            <button type="button" onClick={() => this.guess(value)}>
+                                {value}
+                            </button>
+                        );
+                    })}
+                </div>
             </div>
         );
+    }
+    render({}, { number, fullscreen }) {
+        const fullscreenClass = fullscreen === undefined ? "" : fullscreen;
+        return (
+            <div className={"fullscreen " + fullscreenClass}>
+                {{success: <Success/>, fail: <Fail/>}[fullscreen]}
+                {this.renderInner(number)}
+            </div>
+        );
+    }
+}
+
+export default class App extends Component {
+    render() {
+        return <div className="fullscreen"><Game/></div>;
     }
 }
